65667f72b8d0414bf385555a4dda8ce65c76b908
andrew-1237 <akelly@beechhallschool.org>
2024-09-17 14:49:48 +0100

    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app")
 st.write("my first app")
 name = st.text_input("enter your name")
 st.write("welcome", name)
+
+def menu():
+  slect = st.writle_input("enter L to log in or R to register").lower()
+  if slect == "l":
+    login()
+  elif slect == "r":
+    register()
+  else:
+    menu()
