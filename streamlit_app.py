39036c24d8d4e485311970e98b08a3bb9f6483a0
andrew-1237 <akelly@beechhallschool.org>
2024-09-18 08:51:09 +0100

    added register

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,18 @@ def menu():
     register()
   else:
     menu()
-
+def register():
+  surname = st.text_input("enter your surname").lower()
+  forename = st.text_input("enter your forename").lower()
+  birthday = st.int_input("enter your birthday like this 24031988 for someone born on 24 March 1988")
+  password = st.text_input("enter a password")
+  username = forename[0] + surname[1] + surname[2] + birthday
+  st.write("your user name is " + username)
+  st.write("your password is " + password)
+  file=open("userlist.txt","a")
+  file.write("\n" + username + "," + password + "," + forename + "," + birthday + "," + surname + "," + "no" + "," + "1" + ",")
+  file.close()
+  menu()
 
 
 
