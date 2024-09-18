85e3d33a8fbfea6e9873ffada5caa940e212c8fb
andrew-1237 <akelly@beechhallschool.org>
2024-09-18 08:58:18 +0100

    changed num to number

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def menu():
 def register():
   surname = st.text_input("enter your surname").lower()
   forename = st.text_input("enter your forename").lower()
-  birthday = st.num_input("enter your birthday like this 24031988 for someone born on 24 March 1988")
+  birthday = st.number_input("enter your birthday like this 24031988 for someone born on 24 March 1988")
   password = st.text_input("enter a password")
   username = forename[0] + surname[1] + surname[2] + birthday
   st.write("your user name is " + username)
